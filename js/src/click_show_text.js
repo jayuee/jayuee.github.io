ee2fc8f0a39c1e594f2c1fdcabae930be0b701e7
jayuee <jayuee@163.com>
2020-04-15 16:36:16 +0800

    Site updated: 2020-04-15 16:35:55

--- js/src/click_show_text.js
+++ js/src/click_show_text.js
@@ -1,3 +1,3 @@
-// build time:Tue Apr 14 2020 15:18:02 GMT+0800 (GMT+08:00)
+// build time:Wed Apr 15 2020 16:35:53 GMT+0800 (GMT+08:00)
 var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(t){var e=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");var o=a("<span/>").text(e[a_idx]);a_idx=(a_idx+1)%e.length;var n=t.pageX,i=t.pageY;o.css({"z-index":5,top:i-20,left:n,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"});a("body").append(o);o.animate({top:i-180,opacity:0},3e3,function(){o.remove()})});setTimeout("delay()",2e3)});function delay(){$(".buryit").removeAttr("onclick")}
 //rebuild by neat 
